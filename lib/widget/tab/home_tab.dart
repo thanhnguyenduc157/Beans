0e2240f419544b076ce1d14cfd3fc021a3621538
trisnguyen <trisnguyen1210@gmail.com>
2021-06-07 21:33:12 +0700

    sua button

--- lib/widget/tab/home_tab.dart
+++ lib/widget/tab/home_tab.dart
@@ -121,15 +121,53 @@ class HomeTab extends StatelessWidget {
                     width:MediaQuery.of(context).size.width,
                     height:3,
                   ),
-                  Column(
-                    children:<Widget>[
-                      new Image.asset(
-                        'assets/challengeviewbottom.png',
-                        fit:BoxFit.fill,
-                        width: MediaQuery.of(context).size.width,
+                 new InkWell(
+                    onTap: (){
+                      return Container(
+                      height: 350,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Container(
+                            color: Colors.orangeAccent,
+                            width:MediaQuery.of(context).size.width,
+                            height:3,
+                          ),
+                          new Container(
+                            padding: const EdgeInsets.all(0.0),
+                            height: 30,
+                            width: 60,
+                            decoration: new BoxDecoration(
+                                color:Colors.orangeAccent,
+                                borderRadius: new BorderRadius.only(
+                                  bottomRight: const Radius.circular(50),
+                                  bottomLeft: const Radius.circular(50),
+                                )
+                            ),
+                            child: new IconButton(
+                              icon: Icon(Icons.keyboard_arrow_down),
+                              iconSize:35.0,
+                              color: const Color(0XFFffffff),
+                              padding: const EdgeInsets.all(0.0),
+                              alignment: Alignment.center,
+                              onPressed: () =>Navigator.pop(context),
+                            ),
+                          ),
+                          ChangeNotifierProvider(
+                            create:(context) => ChallengeProvider(),
+                            child: ChallengeView(),
+                          ),
+                        ],
                       ),
-                    ],
+                    );
+                      },
+                    child: new Image.asset(
+                    'assets/challengeviewbottom.png',
+                    fit:BoxFit.fill,
+                    width: MediaQuery.of(context).size.width,
                   ),
+                 ),
                 ],
               ),
             ),
